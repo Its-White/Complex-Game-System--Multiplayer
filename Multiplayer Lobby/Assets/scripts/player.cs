0a7a68d422ad58ea2783cb0b7e5751020ee63d4a
Alex Armstrong <s171302@ad.aie.edu>
2017-05-05 14:50:01 +1000

    main push

---
+++ Multiplayer Lobby/Assets/scripts/player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+ 
+    public float movSpeed = 10.0f;
+    public float rotationSpeed = 100.0f; //sensitivity
+    public float jumpSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float translation = Input.GetAxis("Vertical") * movSpeed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        float jump = Input.GetAxis("Jump") * jumpSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+        transform.Translate(0, jump, 0);
+    
+
+	}
+}
